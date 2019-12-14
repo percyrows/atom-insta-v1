cd5489b3ac438fc66c7744410878c5007af7df64
percyrows <perciano0@yahoo.com.mx>
2019-12-14 14:58:50 -0600

    Fin dia catorce

---
+++ src/pages/home.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+
+class Home extends Component {
+    render() {
+      return ( 
+        <div className="container">
+          <button className="is-Succes">
+          Hola
+          </button>
+        </div>
+  
+      );
+    }
+  }
+  
+  export default Home;
